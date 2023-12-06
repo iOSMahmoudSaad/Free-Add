96427c4156949c53731fc0ede0149dc2d48cc9a5
Mahmoud Saad <msaaad202020@gmail.com>
2023-12-06 17:52:44 +0200

    Create Header

--- Free_Add/Free_Add/Categories/Controller/CategoriesVC.swift
+++ Free_Add/Free_Add/Categories/Controller/CategoriesVC.swift
@@ -9,13 +9,52 @@ import UIKit
 
 class CategoriesVC: UIViewController {
 
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var messageButton: UIButton!
+    
+    @IBOutlet weak var videoButton: UIButton!
+    
+    @IBOutlet weak var profileButton: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
        
     }
 
 
-     
-
+    @IBAction func backButtonTapped(_ sender: Any) {
+        
+        print("Back")
+    }
+    
+    @IBAction func messageButtonTapped(_ sender: Any) {
+        
+        print("mesaage")
+    }
+    
+    @IBAction func videoButtonTapped(_ sender: Any) {
+        
+        print("video")
+    }
+    
+    
+    @IBAction func profileButtonTapped(_ sender: Any) {
+        
+        
+        print("profile")
+    }
+    
+    @IBAction func FilterButtonTapped(_ sender: Any) {
+        
+        print("filter")
+    }
+    
+    @IBAction func scanButtonTapped(_ sender: Any) {
+        
+        print("scan")
+    }
+    
 }
